872b00e394c355f8b95d23891115dbbd5481f878
Alpesh Rathava <alpeshrathava301@gmail.com>
2023-03-01 16:17:08 +0530

    Initial commit

---
+++ app/src/main/java/com/alpesh1/practice/MainActivity.java
@@ -0,0 +1,23 @@
+package com.alpesh1.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtView = findViewById(R.id.txtView);
+
+        txtView.setSelected(true);
+    }
+}
